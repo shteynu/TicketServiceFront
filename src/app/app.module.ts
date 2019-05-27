2bfbe5ebb4ec12b96c88af744367dd56ca687a53
ConstantinePetrash <pca173@yahoo.com>
2019-05-27 21:08:37 +0300

    add component admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HallsComponent } from './components/halls/halls.component';
 import { EventsComponent } from './components/events/events.component';
+import { AdminComponent } from './components/admin/admin.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatMenuModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HallsComponent,
-    EventsComponent
+    EventsComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatMenuModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
